073f0f7035ded267f8cfd84ec2ae722a971fcb73
Tom Lee <tomlee1564@gmail.com>
2021-10-20 16:14:46 +1100

    new function for api request
    new function for api request that spits out array instead of string

--- app/index.js
+++ app/index.js
@@ -1,26 +1,26 @@
-const apiKey = '48c123adbe84d06b9390564c28f11e6a'
+const apiKey = 'eecb346faf3da5f29b487b99c291e0b8'
+// 48c123adbe84d06b9390564c28f11e6a
+// 93f21f1aca9ebdef4701de35fc8354e5
+// eecb346faf3da5f29b487b99c291e0b8 
 
-function getCurrentPosition(){
-    if(navigator.geolocation){
+function getCurrentPosition() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition)
-    } else{
+    } else {
         console.log("Geo location not supported :(")
     }
 }
-function showPosition(position){
+
+async function showPosition(position) {
     let longitude = position.coords.longitude
     let latitude = position.coords.latitude
-    loadApiData()
-    function loadApiData(){
-        const xhttp = new XMLHttpRequest()
-        xhttp.onload = function(){
-            let response = xhttp.response
-            response.forEach(uvData => {
-                
-            });
+    const response = await fetch(`https://api.openuv.io/api/v1/forecast?lat=${latitude}&lng=${longitude}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': apiKey,
         }
-        xhttp.open("GET", `https://api.openuv.io/api/v1/forecast?lat=${latitude}&lng=${longitude}`, true)
-        xhttp.setRequestHeader('x-access-token', apiKey)
-        xhttp.send()
-    }
+    });
+    const json = await response.json(); // parses JSON response into native JavaScript objects
+    console.log(json);
 }
