b28d14818d6f68fe571873dcf0ef7947ba930038
Tom Lee <tomlee1564@gmail.com>
2021-11-01 22:12:23 +1100

    update

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ const apiKey = 'eecb346faf3da5f29b487b99c291e0b8'
 // 48c123adbe84d06b9390564c28f11e6a
 // 93f21f1aca9ebdef4701de35fc8354e5
 // eecb346faf3da5f29b487b99c291e0b8 
+const uvHtml = document.getElementById('uv')
 
 function getCurrentPosition() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition)
     } else {
-        console.log("Geo location not supported :(")
+        console.log("Geo location not supported")
     }
 }
 
@@ -21,6 +22,16 @@ async function showPosition(position) {
             'x-access-token': apiKey,
         }
     });
-    const json = await response.json(); // parses JSON response into native JavaScript objects
-    console.log(json);
+    const uvDataArray = await response.json()
+    uvDataArray.result.forEach(element => {
+        let date = new Date(element.uv_time)
+        let hour = date.getHours()
+        let seconds = date.getSeconds()
+        uvHtml.innerHTML += element.uv + " UV - " + hour + " | "
+    });
+    console.log(uvDataArray.result)
 }
+
+date = new Date('2021-11-01T02:12:51.233Z')
+year = date.getHours()
+console.log(year)
